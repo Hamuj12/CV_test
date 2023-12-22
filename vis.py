dab9ff6e26ce756d30463ef1055d4a0bcf033494
Hamuj12 <hamza.mujtaba@utexas.edu>
2023-12-22 16:30:06 -0600

    update frame for kps

--- vis.py
+++ vis.py
@@ -6,22 +6,24 @@ def visualize_box_and_keypoints(keypoints_3d, box_dimensions, label_walls=True):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
-    # Offsets to center the origin on one face of the box
-    x_offset = -box_dimensions[0] / 2
+    # Offsets to center the origin
+    y_offset = -box_dimensions[0] / 2
+    x_offset = 0
     z_offset = -box_dimensions[2] / 2
 
-    # Drawing the box with the origin at the center of the XZ face
-    for x in [x_offset, -x_offset]:
+    # Drawing the box with new orientation
+    for y in [y_offset, -y_offset]:
         for z in [z_offset, -z_offset]:
-            ax.plot([x, x], [0, box_dimensions[1]], [z, z], color='b')
+            ax.plot([x_offset, box_dimensions[1]], [y, y], [z, z], color='b')
 
-    for y in [0, box_dimensions[1]]:
+    for x in [x_offset, box_dimensions[1]]:
         for z in [z_offset, -z_offset]:
-            ax.plot([x_offset, -x_offset], [y, y], [z, z], color='b')
+            ax.plot([x, x], [y_offset, -y_offset], [z, z], color='b')
 
-    for y in [0, box_dimensions[1]]:
-        for x in [x_offset, -x_offset]:
+    for x in [x_offset, box_dimensions[1]]:
+        for y in [y_offset, -y_offset]:
             ax.plot([x, x], [y, y], [z_offset, -z_offset], color='b')
+
 
     
     # Define colors for each wall
@@ -34,21 +36,23 @@ def visualize_box_and_keypoints(keypoints_3d, box_dimensions, label_walls=True):
         ax.text(x, y, z, f"{i}", color='black', fontsize=9)
 
     # Plotting origin
-    ax.scatter(0, 0, 0, color='g', s=100)  # s is the size of the point
+    ax.scatter(0, 74.5, 0, color='black', s=100)  # s is the size of the point
 
     if label_walls:
-        # Adding labels for each face
-        ax.text(0, 0, 0, "Wall 3", color='blue')
-        ax.text(0, box_dimensions[1], 0, "Wall 1", color='blue')
-        ax.text(0, box_dimensions[1]/2, box_dimensions[2]/2, "Top Wall", color='blue')
-        ax.text(0, box_dimensions[1]/2, -box_dimensions[2]/2, "Bottom Wall", color='blue')
-        ax.text(-box_dimensions[0]/2, box_dimensions[1]/2, 0, "Wall 2", color='blue')
-        ax.text(box_dimensions[0]/2, box_dimensions[1]/2, 0, "Wall 4", color='blue')
+        # Assuming Wall 1 is now facing the positive X direction, etc.
+        ax.text(0, 0, 0, "Wall 3", color='blue')  # Center of Wall 3
+        ax.text(box_dimensions[1], 0, 0, "Wall 1", color='blue')  # Center of Wall 1
+        ax.text(0, box_dimensions[0] / 2, 0, "Wall 2", color='blue')  # Center of Wall 2
+        ax.text(0, -box_dimensions[0] / 2, 0, "Wall 4", color='blue')  # Center of Wall 4
+        ax.text(0, 0, box_dimensions[2] / 2, "Top Wall", color='blue')  # Center of Top Wall
+        ax.text(0, 0, -box_dimensions[2] / 2, "Bottom Wall", color='blue')  # Center of Bottom Wall
+
 
     # Setting axes limits
-    ax.set_xlim([x_offset, -x_offset])
-    ax.set_ylim([0, box_dimensions[1]])
-    ax.set_zlim([z_offset, -z_offset])
+    ax.set_xlim([0, box_dimensions[1]])  # New X (inward)
+    ax.set_ylim([-box_dimensions[0] / 2, box_dimensions[0] / 2])  # New Y (to the left)
+    ax.set_zlim([-box_dimensions[2] / 2, box_dimensions[2] / 2])  # Z (upward)
+
 
     # Setting labels and title
     ax.set_xlabel('X Axis')
@@ -141,11 +145,51 @@ keypoints_3d_example = [
                         (64.5, 37, -101.5),
                         ]
 
+#keypoints_3d_example_transformed = [(y, -x, z) for x, y, z in keypoints_3d_example]
+
+keypoints_3d_example_transformed =  [
+                                    (203, 0, 0), 
+                                    (203, -74.5, 74.5), 
+                                    (203, 74.5, 74.5), 
+                                    (203, 74.5, -74.5), 
+                                    (203, -74.5, -74.5), 
+                                    
+                                    (121.5, 101.5, 0), 
+                                    (156, 101.5, 54.5), 
+                                    (47, 101.5, 54.5), 
+                                    (47, 101.5, -54.5), 
+                                    (156, 101.5, -54.5), 
+                                    
+                                    (0, 0, 0), 
+                                    (0, 74.5, 74.5), 
+                                    (0, -74.5, 74.5), 
+                                    (0, -74.5, -74.5), 
+                                    (0, 74.5, -74.5), 
+                                    
+                                    (121.5, -101.5, 0), 
+                                    (47, -101.5, 54.5), 
+                                    (156, -101.5, 54.5), 
+                                    (156, -101.5, -54.5), 
+                                    (47, -101.5, -54.5), 
+                                    
+                                    (91.5, -10, 101.5), 
+                                    (166, 64.5, 101.5), 
+                                    (166, -64.5, 101.5), 
+                                    (37, -64.5, 101.5), 
+                                    (37, 64.5, 101.5), 
+
+                                    (91.5, -10, -101.5), 
+                                    (166, -64.5, -101.5), 
+                                    (166, 64.5, -101.5), 
+                                    (37, 64.5, -101.5), 
+                                    (37, -64.5, -101.5)
+                                    ]
+
 # save these keypoints as a numpy array
-np.save('calib_box_v2.npy', keypoints_3d_example)
+np.save('calib_box_v2.npy', keypoints_3d_example_transformed)
 
 box_dimensions_example = (203, 203, 203)  # your box dimensions
-visualize_box_and_keypoints(keypoints_3d_example, box_dimensions_example, label_walls=True)
+visualize_box_and_keypoints(keypoints_3d_example_transformed, box_dimensions_example, label_walls=True)
 
 
 # https://www.festi.info/boxes.py/ClosedBox?FingerJoint_angle=90.0&FingerJoint_style=rectangular&FingerJoint_surroundingspaces=2.0&FingerJoint_bottom_lip=0.0&FingerJoint_edge_width=1.0&FingerJoint_extra_length=0.0&FingerJoint_finger=2.0&FingerJoint_play=0.0&FingerJoint_space=2.0&FingerJoint_width=1.0&x=355&y=254&h=102&outside=0&outside=1&thickness=3.0&format=svg&tabs=0.0&qr_code=0&debug=0&labels=0&labels=1&reference=0&inner_corners=loop&burn=0.1&language=en&render=0
